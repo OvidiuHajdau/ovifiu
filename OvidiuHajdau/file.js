3bbd3153964d4916576f33aaf61aaf2ab0048e61
OvidiuHajdau <ovidiuhajdau2029@gmail.com>
2024-09-07 13:31:38 +0300

    Add files via upload

---
+++ OvidiuHajdau/file.js
@@ -0,0 +1,9 @@
+console.log("Hello")
+let name = "Ovidiu"
+console.log(name)
+let person_name = "Ion"
+console.log(person_name)
+person_name = "Danu"
+console.log(person_name)
+imya = "Andrei Golban"
+console.log(imya)
